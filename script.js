2237a0225931a7076ff416b62381c219f7f87aa8
Jeff Shapiro <MacArthur1@MacArthur-Jeffs-MacBook-Pro.local>
2015-05-03 15:11:01 -0400

    hw completed

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 var main = function() {
   $('form').submit(function() {
-    
+
+  	var comment = $('#comment').val();
+
+
     if(comment !== "") {
-      var html = $('<li>').text(comment);
-      
+      var html = $('<li>').text(comment).prependTo('.comments');  
     }
 
+    $('#comment').val("");
     return false;
+
+
   });
 };
-
 $(document).ready(main);
